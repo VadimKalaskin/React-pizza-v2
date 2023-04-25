c642738ddbdd3cdffdac5cd74114288e5db6e154
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-25 13:50:45 +0300

    Разбор html на компоненты

--- react-pizza-v2/src/App.js
+++ react-pizza-v2/src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
+import Sort from './components/Sort/Sort';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 
 function App() {
   return (
-    <>
-      <h1> hello world</h1>
-    </>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={395} />
+            <PizzaBlock title="Мясная" price={565} />
+            <PizzaBlock title="Ароматная" price={495} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
-
-

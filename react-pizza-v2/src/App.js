db088c7b99fa2c5350ddf2cfb2ff4bc9b09c1215
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-28 17:41:05 +0300

    Вопрос по контексту

--- react-pizza-v2/src/App.js
+++ react-pizza-v2/src/App.js
@@ -11,21 +11,25 @@ import Header from './components/Header';
 // https://642c3132208dfe25472a75cf.mockapi.io/items Для корзины
 // https://642c3132208dfe25472a75cf.mockapi.io/pizzas Пиццы
 
+export const SearchContext = React.createContext();
+
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home searchValue={searchValue} />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
